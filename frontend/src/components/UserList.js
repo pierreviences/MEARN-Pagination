efc2778687e9089dcde174929c514a5df99afcc6
Muhamad Yazid Imani <imaniyazid@gmail.com>
2023-07-29 14:17:16 +0700

    Final Project

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import ReactPaginate from 'react-paginate'
 const UserList = () => {
     const [users, setUser] = useState([])
     const [page, setPage] = useState(0)
@@ -7,31 +8,45 @@ const UserList = () => {
     const [pages, setPages] = useState(0)
     const [rows, setRows] = useState(0)
     const [keyword, setKeyword] = useState("")
+    const [query, setQuery] = useState("")
+    const [msg, setMsg] = useState("")
 
     useEffect(() => {
-        console.log("asd")
         getUsers()
     }, [page, keyword])
 
     const getUsers = async () => {
-        console.log('as')
         const response = await axios.get(`http://localhost:5000/users?search_query=${keyword}&page=${page}&limit=${limit}`)
-        console.log(response)
         setUser(response.data.result)
         setPage(response.data.page)
         setPages(response.data.totalPage)
         setRows(response.data.totalRows)
     }
 
+    const changePage = ({ selected }) => {
+        setPage(selected)
+        if (selected === 9) {
+            setMsg("Jika tidak menemukan data yang anda cari, silahkan cari data dengan kata kunci spesifik!")
+        } else {
+            setMsg("")
+        }
+    }
+
+    const searchData = (e) => {
+        e.preventDefault()
+        setPage(0)
+        setKeyword(query)
+
+    }
 
     return (
         <div className="container mt-5">
             <div className="columns">
                 <div className="column is-center">
-                    <form>
+                    <form onSubmit={searchData}>
                         <div className='field has-addons'>
                             <div className="control is-expanded">
-                                <input type="text" className='input' placeholder='Find something here...' />
+                                <input type="text" className='input' value={query} onChange={(e) => setQuery(e.target.value)} placeholder='Find something here...' />
                             </div>
                             <div className='control'>
                                 <button type='submit' className='button is-info'>Search</button>
@@ -50,15 +65,32 @@ const UserList = () => {
                         <tbody>
                             {users.map(user => (
                                 <tr key={user.id}>
-                                    <th>{user.id}</th>
-                                    <th>{user.name}</th>
-                                    <th>{user.email}</th>
-                                    <th>{user.gender}</th>
+                                    <td>{user.id}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.gender}</td>
                                 </tr>
                             ))}
 
                         </tbody>
                     </table>
+                    <p>Total Rows: {rows} Page: {rows ? page + 1 : 0} of {pages}</p>
+                    <p className='has-text-centered has-text-danger'>{msg}</p>
+                    <nav class="pagination is-centered" key={rows} role="navigation" aria-label="pagination">
+                        <ReactPaginate
+                            nextLabel={"Next >"}
+                            pageCount={Math.min(10, pages)}
+                            previousLabel={"< Prev"}
+                            onPageChange={changePage}
+                            containerClassName={"pagination-list"}
+                            pageLinkClassName={"pagination-link"}
+                            previousLinkClassName={"pagination-previous"}
+                            nextLinkClassName={"pagination-next"}
+                            activeLinkClassName={"pagination-link is-current"}
+                            disabledLinkClassName={"pagination-link is-disabled"}
+
+                        />
+                    </nav>
                 </div>
             </div>
         </div>
