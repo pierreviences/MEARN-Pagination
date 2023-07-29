ad1b1387c2cb542bf5a894ff788dd9feb3566743
Muhamad Yazid Imani <imaniyazid@gmail.com>
2023-07-29 13:57:14 +0700

    Get Data API with axios

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import UserRoutes from './routes/UserRoute.js'
 const app = express()
 
-// app.use(cors)
+app.use(cors()) // kalo di web nyalain ini
 app.use(express.json())
 app.use(UserRoutes)
 
--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -1,6 +1,29 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 const UserList = () => {
+    const [users, setUser] = useState([])
+    const [page, setPage] = useState(0)
+    const [limit, setLimit] = useState(10)
+    const [pages, setPages] = useState(0)
+    const [rows, setRows] = useState(0)
+    const [keyword, setKeyword] = useState("")
+
+    useEffect(() => {
+        console.log("asd")
+        getUsers()
+    }, [page, keyword])
+
+    const getUsers = async () => {
+        console.log('as')
+        const response = await axios.get(`http://localhost:5000/users?search_query=${keyword}&page=${page}&limit=${limit}`)
+        console.log(response)
+        setUser(response.data.result)
+        setPage(response.data.page)
+        setPages(response.data.totalPage)
+        setRows(response.data.totalRows)
+    }
+
+
     return (
         <div className="container mt-5">
             <div className="columns">
@@ -25,12 +48,15 @@ const UserList = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
+                            {users.map(user => (
+                                <tr key={user.id}>
+                                    <th>{user.id}</th>
+                                    <th>{user.name}</th>
+                                    <th>{user.email}</th>
+                                    <th>{user.gender}</th>
+                                </tr>
+                            ))}
+
                         </tbody>
                     </table>
                 </div>
